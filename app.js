2a687fff3dae3dfdee4dafac7a6769deb3c77553
hawrot <mhawro200@caledonian.ac.uk>
2019-08-01 12:40:33 +0100

    Redirecting requests fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
     const url = req.url;
-    const method = req.methodl
+    const method = req.method;
 
     if (url === '/'){
         res.setHeader('Content-Type', 'text/html');
@@ -16,8 +16,11 @@ const server = http.createServer((req, res) =>{
         return res.end();
 
     }
-    if (url === '/message' && method === POST){
-
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
