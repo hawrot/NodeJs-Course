d07f3a7011ca982933df5ad7eac78dc1dfd5f150
hawrot <mhawro200@caledonian.ac.uk>
2019-08-01 14:53:47 +0100

    Import routes from routes.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
+
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-});
+const server = http.createServer(routes);
 
 
 server.listen(3000);  ///server keeps listening to the requests
