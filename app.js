33bbf5d1ed93e8f765b0a91025087853cc9b90d1
hawrot <mhawro200@caledonian.ac.uk>
2019-08-01 12:35:51 +0100

    Add another if statement for /message

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const http = require('http');
 
 const server = http.createServer((req, res) =>{
     const url = req.url;
+    const method = req.methodl
 
-    if (irl === '/'){
+    if (url === '/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="post"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="post"><input name="message" type="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.end();
+        return res.end();
 
     }
+    if (url === '/message' && method === POST){
 
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Page</title></head>');
