253d333ca0acee78aa855016d40e8355ce54ab5a
hawrot <mhawro200@caledonian.ac.uk>
2019-08-01 12:27:20 +0100

    Adding first response from the server

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) =>{
-    console.log(req);
-    console.log('hello from the server');
+    console.log(req.url, req.method, req.header);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Page</title></head>');
+    res.write('<body><h1>Page 1</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 
-server.listen(3000);
+server.listen(3000);  ///server keeps listening to the requests
