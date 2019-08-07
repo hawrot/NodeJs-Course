8d4957db6ac5519518e6ba22b5b5ba9b6c03ef13
hawrot <mhawro200@caledonian.ac.uk>
2019-08-07 14:23:29 +0100

    Adding the paths

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express =require('express');
 
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log('In the middleware');
-    next();  //Allows the request to continue to the next middleware in line
-});
 
-app.use((req,res,next) =>{
+app.use('/add-product',(req,res,next) =>{
+    console.log('In the another middleware');
+    res.send('<h1>Hello from express  - product page</h1>');
+});
+app.use('/',(req,res,next) =>{
     console.log('In the another middleware');
     res.send('<h1>Hello from express</h1>');
 });
