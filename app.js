4dd5b2685b30f0b20cae2faf03beff859c7aa112
hawrot <mhawro200@caledonian.ac.uk>
2019-08-07 13:58:19 +0100

    Log the middelware in the console

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const express =require('express');
 
 const app = express();
 
+app.use((req,res,next) =>{
+    console.log('In the middelware');
+});
 
 const server = http.createServer(app);
 
 
 server.listen(3000);  ///server keeps listening to the requests
+
