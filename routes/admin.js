d6b9e9655543de84891d252268b79815585e4a03
hawrot <mhawro200@caledonian.ac.uk>
2019-08-12 23:58:15 +0100

    Add styling

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next) =>{
     console.log('In the another middleware');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir,  'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../util/path');
 
 
 
 router.use('/',(req,res,next) =>{
     console.log('In the another middleware');
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
