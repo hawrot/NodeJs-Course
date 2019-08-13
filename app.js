3752bc9da8d0ce67819c98cea3e6b62cc6d5b65a
hawrot <mhawro200@caledonian.ac.uk>
2019-08-13 12:31:23 +0100

    Add shop.pug file

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
