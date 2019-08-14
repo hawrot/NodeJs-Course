587a95c3f2f35257f66885be8bf35f25c9e03132
hawrot <mhawro200@caledonian.ac.uk>
2019-08-15 00:06:16 +0100

    Helper added

--- models/product.js
+++ models/product.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = [];
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 
 module.exports = class Product {
-    constructor(title){
-        this.title = title;
+    constructor(t) {
+        this.title = t;
     }
-    save(){
-       // products.push(this);
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+
+    save() {
+        getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=>{
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
             });
         });
     }
 
-    static fetchAll(cb){
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent)=>{
-           if(err){
-               cb([]);
-           }
-            cb(JSON.parse(fileContent));
-        });
-     //   return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 };
