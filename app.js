b381f0e1d73680b3c506f67810d793c3d31b5ca1
hawrot <mhawro200@caledonian.ac.uk>
2019-08-14 13:03:20 +0100

    404 page does not works

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 
@@ -18,9 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page not found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
