9eb3a22b8ea54cbe77973b46370f96a9f2a01a97
hawrot <mhawro200@caledonian.ac.uk>
2019-08-25 22:39:55 +0100

    Restructure directories

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product test',
         path: '/admin/add-product',
         formsCSS: true,
@@ -20,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     const products = Product.fetchAll((products)=>{
-        res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true})
+        res.render('shop/product-list', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true})
     });
 
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
