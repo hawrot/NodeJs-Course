4509f0c5b036cf9150325b5fb82d3c4520da81c8
hawrot <mhawro200@caledonian.ac.uk>
2019-08-27 13:53:26 +0100

    Add route for product id

--- routes/shop.js
+++ routes/shop.js
@@ -14,5 +14,7 @@ router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.get('/products/:productId');   //:productId says to express that the value will be passed
+
 
 module.exports = router;
