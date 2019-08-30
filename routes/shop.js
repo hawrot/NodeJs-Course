229f569762799a8b7b90ded6525899bf0e4cc07f
hawrot <mhawro200@caledonian.ac.uk>
2019-08-30 14:00:48 +0100

    Deleting product from the cart - issue - deleting all the products no matters about the quantity

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,7 @@ router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.GetCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 router.get('/products/:productId', shopController.getProduct);   //:productId says to express that the value will be passed
 
 router.post('/cart', shopController.postCart);
