98a8e51435b0024d3142010ad571b554646d4d6f
hawrot <mhawro200@caledonian.ac.uk>
2019-09-02 15:32:03 +0100

    Connect to mongodb

--- util/database.js
+++ util/database.js
@@ -2,10 +2,16 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb+srv://matt:A6WfFNLQgzPAn4Ej@cluster0-inhjh.mongodb.net/test?retryWrites=true&w=majority')
-    .then(result =>{
-        console.log('Connected!');
-    })
-    .catch(err =>{
-        console.log(err);
-    });
+const MongoConnect = callback =>{
+    MongoClient.connect('mongodb+srv://matt:A6WfFNLQgzPAn4Ej@cluster0-inhjh.mongodb.net/test?retryWrites=true&w=majority')
+        .then(result =>{
+            console.log('Connected!');
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+
+};
+
+module.exports = MongoConnect;
+
