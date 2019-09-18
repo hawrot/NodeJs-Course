570f6ff036648c5e35f3b81c439ebfb163c097ca
hawrot <mhawro200@caledonian.ac.uk>
2019-09-18 13:40:47 +0100

    Imported code from udemy

--- util/database.js
+++ util/database.js
@@ -1,33 +1,29 @@
 const mongodb = require('mongodb');
-
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const MongoConnect = callback =>{
-    MongoClient.connect('mongodb+srv://matt:A6WfFNLQgzPAn4Ej@cluster0-inhjh.mongodb.net/test?retryWrites=true&w=majority')
-        .then(client =>{
-            console.log('Connected!');
-            _db = client.db();
-            callback();
-        })
-        .catch(err =>{
-            console.log(err);
-            throw err;
-        });
-
+const mongoConnect = callback => {
+  MongoClient.connect(
+      'mongodb+srv://matt:RVHI1ttIVsQUvZsR@cluster0-inhjh.mongodb.net/test?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-const getDb = () =>{
-    if (_db){
-        return _db;
-    }
-    throw 'No db found';
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
 };
 
-
-exports.MongoConnect = MongoConnect;
+exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
-
-
