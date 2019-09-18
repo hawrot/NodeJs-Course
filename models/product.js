cce12833e796a66ff7dc547a3ed92da462a4d6f0
hawrot <mhawro200@caledonian.ac.uk>
2019-09-18 13:10:51 +0100

    Implement update method

--- models/product.js
+++ models/product.js
@@ -2,16 +2,27 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
+    constructor(title, imageUrl, description, price, id) {
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
+        this._id = id;
     }
+
     save(){
         const db = getDb();
-       return db.collection('products').insertOne(this).then(result =>{
+        let dbOp;
+
+        if (this._id){
+
+            //Update the product
+            dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+        } else {
+            dbOp = db.collection('products').insertOne(this);
+
+        }
+       return dbOp.then(result =>{
             console.log(result);
         })
             .catch(err =>{
