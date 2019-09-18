08fe00fc4608d889e3ca9db7474c00e4480ba853
hawrot <mhawro200@caledonian.ac.uk>
2019-09-18 12:59:46 +0100

    Adjust getEditProduct method

--- controllers/admin.js
+++ controllers/admin.js
@@ -43,10 +43,12 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
+   Product.findById(prodId)
+       .then(products => {
+        const product = products[0];
+        if (!product){
             return res.redirect('/');
-        }
+       }
         res.render('admin/edit-product', {
             pageTitle: 'Edit Product',
             path: '/admin/edit-product',
