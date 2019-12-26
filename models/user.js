b5895e42b83b277ab40b0da0f1153397f028a2f0
hawrot <mhawro200@caledonian.ac.uk>
2019-12-26 23:21:55 +0000

    Create user schema

--- models/user.js
+++ models/user.js
@@ -1,26 +1,28 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    }
+const userSchema = new Schema({
+   name: {
+       type: String,
+       required: true
+   },
+    email: {
+       type: String,
+       required: true
+   },
+    cart: {
+        items:
+        [
+            {
+                productId: {type: Schema.Types.ObjectId, required: true},
+                quantity: {type: Number, required: true }
 
+           }
+        ]
+   }
 });
+
+module.exports = mongoose.model('User', userSchema);
 
 
 
