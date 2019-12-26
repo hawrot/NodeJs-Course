f4b0c1770a12af01b250d70ed24978b62ea94c18
hawrot <mhawro200@caledonian.ac.uk>
2019-12-26 23:57:11 +0000

    Display the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,6 +16,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+
   Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
@@ -43,8 +44,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+      .populate('cart.items.productId')
+      .execPopulate()
+    .then(user => {
+        const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
