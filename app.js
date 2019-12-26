e5b618d8a6b792ee8540027b33f0dbb9d9714ad3
hawrot <mhawro200@caledonian.ac.uk>
2019-12-26 22:48:49 +0000

    Files fetched, mongo configured

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById('5d8238009cc94d15b9105039')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findById('5d8372894f269d18d9fadf83')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+  .connect(
+    'mongodb+srv://matt:ppULHJfdq6MjHqcV@cluster0-inhjh.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
     app.listen(3000);
-});
+  })
+  .catch(err => {
+    console.log(err);
+  });
