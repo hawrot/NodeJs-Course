aa7f1144ac75909037757c7286de8ac80f7beb73
hawrot <mhawro200@caledonian.ac.uk>
2020-01-20 15:27:30 +0100

    Thrown an error for add product

--- controllers/admin.js
+++ controllers/admin.js
@@ -53,7 +53,20 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      console.log(err);
+      return res.status(500).render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false,
+        hasError: true,
+        product: {
+          title: title,
+          imageUrl: imageUrl,
+          price: price,
+          description: description
+        },
+        errorMessage: 'Database operation failed',
+        validationErrors: []
+      });
     });
 };
 
