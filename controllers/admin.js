664107ec190c9d93540b60f26855316749588fa9
hawrot <mhawro200@caledonian.ac.uk>
2020-01-20 15:30:51 +0100

    Redicrect to 500 if error occurred

--- controllers/admin.js
+++ controllers/admin.js
@@ -53,20 +53,7 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      return res.status(500).render('admin/add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        editing: false,
-        hasError: true,
-        product: {
-          title: title,
-          imageUrl: imageUrl,
-          price: price,
-          description: description
-        },
-        errorMessage: 'Database operation failed',
-        validationErrors: []
-      });
+      res.redirect('/500');
     });
 };
 
