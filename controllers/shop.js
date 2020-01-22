39700bbed32b2ed18535722036ac61cdea9ea175
hawrot <mhawro200@caledonian.ac.uk>
2020-01-22 16:02:24 +0100

    Finish pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,15 +9,28 @@ const ITEMS_PER_PAGE = 3;
 
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then(products => {
-      console.log(products);
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments().then(numberProducts =>{
+    totalItems = numberProducts;
+    return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE) //pagination
+        .limit(ITEMS_PER_PAGE) //pagination
+  })
+      .then(products => {
+        res.render('shop/product-list', {
+          prods: products,
+          pageTitle: 'Products',
+          path: '/products',
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        })
+      })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
